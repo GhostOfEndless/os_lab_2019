19ad65ac35ee09362be7852c2e371231f1315133
GhostOfEndless <165549721+GhostOfEndless@users.noreply.github.com>
2024-11-14 20:42:40 +0000

    Factorial refactored

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
 
-// Структура для передачи данных в потоки
+// Структура для передачи данных потокам
 typedef struct {
+    int thread_id;
     int start;
     int end;
-    int mod;
-    long long *result;
-    pthread_mutex_t *mutex;
-} ThreadData;
+    long long mod;
+    long long partial_result;
+} thread_data_t;
 
-// Функция для вычисления факториала по модулю
-void *compute_factorial(void *arg) {
-    ThreadData *data = (ThreadData *)arg;
-    long long local_result = 1;
+// Глобальная переменная для результата и мьютекс
+long long result = 1;
+pthread_mutex_t mutex;
 
-    for (int i = data->start; i <= data->end; i++) {
-        local_result = (local_result * i) % data->mod;
+// Функция, выполняемая каждым потоком
+void* compute_partial_factorial(void* arg) {
+    thread_data_t* data = (thread_data_t*)arg;
+    long long partial = 1;
+    for (int i = data->start; i <= data->end; ++i) {
+        partial = (partial * i) % data->mod;
     }
 
-    // Синхронизация доступа к общему результату
-    pthread_mutex_lock(data->mutex);
-    *data->result = (*data->result * local_result) % data->mod;
-    pthread_mutex_unlock(data->mutex);
-
+    // Блокировка мьютекса перед обновлением общего результата
+    pthread_mutex_lock(&mutex);
+    result = (result * partial) % data->mod;
+    pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
-    int k, pnum, mod;
-    int opt;
+int main(int argc, char* argv[]) {
+    int k = -1;
+    int pnum = -1;
+    long long mod = -1;
+    // Определение возможных опций
+    static struct option options[] = {
+        {"k", required_argument, 0, 'k'},
+        {"pnum", required_argument, 0, 'p'},
+        {"mod", required_argument, 0, 'm'},
+        {0, 0, 0, 0}
+    };
 
-    // Чтение параметров командной строки
-    while ((opt = getopt(argc, argv, "k:p:m:")) != -1) {
-        switch (opt) {
+    // Разбор аргументов командной строки
+    while (1) {
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "k:p:m:", options, &option_index);
+        if (c == -1)
+            break;
+        switch (c) {
             case 'k':
                 k = atoi(optarg);
                 break;
@@ -43,53 +58,79 @@ int main(int argc, char *argv[]) {
                 pnum = atoi(optarg);
                 break;
             case 'm':
-                mod = atoi(optarg);
+                mod = atoll(optarg);
                 break;
             default:
-                fprintf(stderr, "Usage: %s -k <number> -p <threads> -m <mod>\n", argv[0]);
-                exit(EXIT_FAILURE);
+                printf("Неправильные аргументы\n");
+                return 1;
         }
     }
 
-    if (k <= 0 || pnum <= 0 || mod <= 0) {
-        fprintf(stderr, "Invalid arguments\n");
-        exit(EXIT_FAILURE);
+    // Проверка корректности введенных параметров
+    if (k < 0 || pnum <= 0 || mod <= 0) {
+        printf("Использование: %s --k <число> --pnum <количество потоков> --mod <модуль>\n", argv[0]);
+        return 1;
     }
 
-    pthread_t threads[pnum];
-    ThreadData data[pnum];
-    pthread_mutex_t mutex;
-    long long result = 1;
-
     // Инициализация мьютекса
-    pthread_mutex_init(&mutex, NULL);
+    if (pthread_mutex_init(&mutex, NULL) != 0) {
+        perror("pthread_mutex_init");
+        return 1;
+    }
 
-    // Разделение задачи на части для каждого потока
-    int chunk_size = k / pnum;
-    int start = 1;
+    pthread_t* threads = malloc(sizeof(pthread_t) * pnum);
+    if (threads == NULL) {
+        perror("malloc");
+        return 1;
+    }
 
-    for (int i = 0; i < pnum; i++) {
-        data[i].start = start;
-        data[i].end = (i == pnum - 1) ? k : start + chunk_size - 1;
-        data[i].mod = mod;
-        data[i].result = &result;
-        data[i].mutex = &mutex;
+    thread_data_t* thread_data = malloc(sizeof(thread_data_t) * pnum);
+    if (thread_data == NULL) {
+        perror("malloc");
+        free(threads);
+        return 1;
+    }
 
-        pthread_create(&threads[i], NULL, compute_factorial, (void *)&data[i]);
-
-        start = data[i].end + 1;
+    // Определение диапазонов для каждого потока
+    int numbers_per_thread = k / pnum;
+    int remaining = k % pnum;
+    int current = 1;
+    for (int i = 0; i < pnum; ++i) {
+        thread_data[i].thread_id = i;
+        thread_data[i].start = current;
+        thread_data[i].end = current + numbers_per_thread - 1;
+        if (remaining > 0) {
+            thread_data[i].end += 1;
+            remaining -= 1;
+        }
+        current = thread_data[i].end + 1;
+        thread_data[i].mod = mod;
+        thread_data[i].partial_result = 1;
+        // Создание потока
+        if (pthread_create(&threads[i], NULL, compute_partial_factorial, &thread_data[i]) != 0) {
+            perror("pthread_create");
+            free(threads);
+            free(thread_data);
+            pthread_mutex_destroy(&mutex);
+            return 1;
+        }
     }
 
     // Ожидание завершения всех потоков
-    for (int i = 0; i < pnum; i++) {
-        pthread_join(threads[i], NULL);
+    for (int i = 0; i < pnum; ++i) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("pthread_join");
+            free(threads);
+            free(thread_data);
+            pthread_mutex_destroy(&mutex);
+            return 1;
+        }
     }
 
-    // Уничтожение мьютекса
+    // Освобождение ресурсов
     pthread_mutex_destroy(&mutex);
-
-    // Вывод результата
-    printf("Factorial of %d modulo %d is: %lld\n", k, mod, result);
-
+    free(threads);
+    free(thread_data);
+    printf("%d! mod %lld = %lld\n", k, mod, result);
     return 0;
 }
