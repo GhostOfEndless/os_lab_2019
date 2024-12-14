0cedff9ba8f990843996a469298c7d4eca61034f
GhostOfEndless <165549721+GhostOfEndless@users.noreply.github.com>
2024-12-14 11:43:29 +0000

    lab 4 fixed

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -3,27 +3,9 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include "sum.h"
 
 #include "utils.h"
-
-struct SumArgs {
-    int *array;
-    int begin;
-    int end;
-};
-
-int Sum(const struct SumArgs *args) {
-    int sum = 0;
-    for (int i = args->begin; i < args->end; i++) {
-        sum += args->array[i];
-    }
-    return sum;
-}
-
-void *ThreadSum(void *args) {
-    struct SumArgs *sum_args = (struct SumArgs *)args;
-    return (void *)(size_t)Sum(sum_args);
-}
 
 int main(int argc, char **argv) {
     uint32_t threads_num = 0;
